bef60c60f1a8cd70d9d61e690103361c8f007e58
saredd <suryaprathap.reddy@planonsoftware.com>
2025-06-27 08:40:57 +0530

    40 days challenge Day:05 Solving TUF recursion and Revision of LinkedList cycle based problems and Tree BFS based problems from Karumanchi

---
+++ revision/trees/narasimhaKarumanchi/PreOrderIterative.java
@@ -0,0 +1,34 @@
+package revision.trees.narasimhaKarumanchi;
+
+import revision.trees.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class PreOrderIterative {
+    public List<Integer> preorder(TreeNode root) {
+        return preorderIterativeHelper(root);
+    }
+
+    public List<Integer> preorderIterativeHelper(TreeNode root) {
+        List<Integer> preOrderList = new ArrayList<>();
+        if (root == null) {
+            return preOrderList;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+
+            TreeNode poppedNode = stack.pop();
+            preOrderList.add(poppedNode.data);
+            if (poppedNode.right != null) {
+                stack.push(poppedNode.right);
+            }
+            if (poppedNode.left != null) {
+                stack.push(poppedNode.left);
+            }
+        }
+        return preOrderList;
+    }
+}
